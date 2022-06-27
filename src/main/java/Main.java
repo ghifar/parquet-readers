5f8c48efe77a4f39ad984acf08babb8f5d596cac
Abudzar Al Ghiffari <abudzar.ghiffari@traveloka.com>
2022-06-27 17:22:59 +0700

    update to generate file to json format

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,7 @@
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.*;
-import org.apache.spark.sql.connector.expressions.Lit;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,21 +17,22 @@ public class Main {
                 .config("spark.sql.caseSensitive", "true").getOrCreate();
 
         Dataset<Row> df = sp.read()
-                .parquet("parquet-directory-or-files")
+                .parquet("/Users/abudzar.ghiffari/Downloads/part-00000-3f5b74c2-b935-4c5a-a507-6fc1b66e22f7.c000.snappy.parquet")
                 .coalesce(1);
 
 
         /* EXAMPLE
-        * Uncomment to try the function */
+         * Uncomment to try the function */
 
         // 1. to print schema only
         df.printSchema();
 
         // 2. example this is to select any columns
+        df.select("*").show();
         //df.select("`de_sales.settlement_invoice.original_attributes`").show(10, false);
 
         // 3. export to csv. output folder should in this directory project root
-        //toCsv(df.filter("`product.product_source_list.vendor_id`"), "*");
+        //generateFile(df.filter("`product.product_source_list.vendor_id`"), "*", CSV);
 
         // 4. to print only contains word in params
         //printContains(df, "de_sales.settlement_invoice.original_attributes");
@@ -74,12 +73,26 @@ public class Main {
      * @param df
      * @param column -> * or name of the column
      */
-    static void toCsv(Dataset<Row> df, String column) {
-        df.select(column)
+    static void generateFile(Dataset<Row> df, String column, OutputFormat outputFormat) {
+        final DataFrameWriter<?> result = df.select(column)
                 .write()
                 .mode(SaveMode.Overwrite)
-                .option("header", "true")
-                .csv("csv-output");
+                .option("header", "true");
 
+        switch (outputFormat) {
+            case CSV:
+                result.csv("csv-output");
+                break;
+            case JSON:
+                result.json("json-output");
+                break;
+            default:
+                System.out.println("Unknown output format!");
+        }
+    }
+
+
+    enum OutputFormat {
+        CSV, JSON
     }
 }
